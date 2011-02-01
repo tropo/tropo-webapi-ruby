16882ca86fe5eb44152bb8ddc83533891af9774f
Jason Goecke <jason@goecke.net>
2011-02-01 08:29:31 -0800

    Modified to use rspec 2.x for unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-%w(rubygems tropo-webapi-ruby hashie spec spec/autorun).each { |lib| require lib }
+%w(rubygems tropo-webapi-ruby hashie rspec rspec/autorun).each { |lib| require lib }
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   
 end
